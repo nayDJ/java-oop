4488a9103e9a98abe3ad2865e40d7b92eed19e63
nayDJ <147808074+nayDJ@users.noreply.github.com>
2024-10-21 03:05:30 +0700

    Update App.java
    1. menambahkan variabel "addQty" pada case 1 untuk menambahkan jumlah barang
    2. menambahkan perkondisian "if(cashier.isEmpty") pada case 2 dan 3, jika belum ada barang yang ditambahkan maka akan menampilkan "Daftar barang kosong. Tambahkan barang terlebih dahulu."
    pada case ke 3 , jika barang sudah di inputkan oleh user, program akan menampilkan display jumlah total barang dan total Harga dan ditambahkan variabel "payment" sebagai proses pembayaran agar user dapat menginputkan jumlah pembayaran

--- java-sample/src/App.java
+++ java-sample/src/App.java
@@ -1,35 +1,52 @@
 import java.util.Scanner;
 
 public class App {
-    @SuppressWarnings("resource")
     public static void main(String[] args) throws Exception {
-        Cashier cashier = new Cashier();
-        //variable "cashier" --> instance of class Cashier 
+        Cashier cashier = new Cashier(); 
         Scanner scanner = new Scanner(System.in);
         while (true) {
+             System.out.println("----------------------------------");
+            System.out.println("|\tProgram Transaksi Kasir\t\t |");
+            System.out.println("----------------------------------");
             System.out.println("1. Tambah Barang");
             System.out.println("2. Hapus Barang");
-            System.out.println("3. Tampilkan Total");
+            System.out.println("3. Tampilkan Total & Bayar");
             System.out.println("4. Keluar");
+            System.out.println("----------------------------------");
             System.out.print("Pilih opsi: ");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
+                    System.out.println("----------------------------------");
                     System.out.print("Masukkan kode barang: ");
                     String addCode = scanner.next();
                     System.out.print("Masukkan nama barang: ");
                     String addName = scanner.next();
                     System.out.print("Masukkan harga barang: ");
-                    double addPrice = scanner.nextDouble();
-                    cashier.addItem(addPrice, addCode, addName);
+                    int addPrice = scanner.nextInt();
+                    System.out.print("Masukkan jumlah barang: ");
+                    int addQty = scanner.nextInt();
+                    cashier.addItem(addPrice, addCode, addName, addQty);
                     break;
                 case 2:
-                    System.out.print("Masukkan kode barang yang akan dihapus: ");
-                    String removeCode = scanner.next();
-                    cashier.removeItem(removeCode);
+                    if (cashier.isEmpty()) {
+                        System.out.println("Daftar barang kosong. Tambahkan barang terlebih dahulu.");
+                    } else {
+                        System.out.print("Masukkan kode barang yang akan dihapus: ");
+                        String removeCode = scanner.next(); 
+                        cashier.removeItem(removeCode); 
+                    }
                     break;
                 case 3:
-                    cashier.displayTotal();
+                    if (cashier.isEmpty()) {
+                        System.out.println("Daftar barang kosong. Tambahkan barang terlebih dahulu.");
+                    } else {
+                        System.out.println("----------------------------------");
+                        cashier.displayTotal(); 
+                        System.out.print("Masukkan jumlah pembayaran: ");
+                        int payment = scanner.nextInt(); 
+                        cashier.processPayment(payment); 
+                    }
                     break;
                 case 4:
                     System.out.println("Terima kasih telah menggunakan sistem kasir kami!");
